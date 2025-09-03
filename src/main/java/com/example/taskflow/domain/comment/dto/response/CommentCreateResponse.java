5d52ef1018b0c65802ff3c7f4534ac417c26fdf1
ksg <kasaga234@gmail.com>
2025-09-03 16:17:34 +0900

    feat(comment): comment create 기능 추가

---
+++ src/main/java/com/example/taskflow/domain/comment/dto/response/CommentCreateResponse.java
@@ -0,0 +1,35 @@
+package com.example.taskflow.domain.comment.dto.response;
+
+import com.example.taskflow.domain.comment.entity.Comment;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+public class CommentCreateResponse {
+    private Long id;
+    private String content;
+    private Long taskId;
+    private Long userId;
+    private CommentUserResponse user;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public static CommentCreateResponse from (Comment comment, CommentUserResponse userResponse) {
+        return new  CommentCreateResponse(
+                comment.getId(),
+                comment.getContent(),
+                comment.getTask().getId(),
+                comment.getUser().getId(),
+                userResponse,
+                comment.getCreatedAt(),
+                comment.getUpdatedAt()
+        );
+    }
+
+
+
+
+}
---
+++ src/main/java/com/example/taskflow/domain/comment/dto/response/CommentUserResponse.java
@@ -0,0 +1,25 @@
+package com.example.taskflow.domain.comment.dto.response;
+
+import com.example.taskflow.domain.comment.entity.Comment;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CommentUserResponse {
+    private Long id;
+    private String userName;
+    private String name;
+    private String email;
+    // private ?? role
+
+    public static CommentUserResponse from(Comment comment) { // comment 엔티티의 작성자 user 정보를 꺼냄
+        return new CommentUserResponse(
+                comment.getUser().getId(),
+                comment.getUser().getUsername(),
+                comment.getUser().getName(),
+                comment.getUser().getEmail()
+        );
+    }
+
+}
