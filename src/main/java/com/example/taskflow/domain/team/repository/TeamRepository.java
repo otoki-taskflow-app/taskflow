f181e9add35254fce5743b59ac73153c7f130c9d
dbdnsdb123 <dbdnsdb123@naver.com>
2025-09-04 11:38:06 +0900

    refactor(teams): fix TeamExternalService,TeamErrorCode

--- src/main/java/com/example/taskflow/domain/team/repository/TeamRepository.java
+++ src/main/java/com/example/taskflow/domain/team/repository/TeamRepository.java
@@ -1,13 +1,16 @@
 package com.example.taskflow.domain.team.repository;
 
-
-import com.example.taskflow.domain.task.exception.InvalidTaskException;
 import com.example.taskflow.domain.team.entity.Team;
+import com.example.taskflow.domain.team.exception.InvalidTeamException;
 import com.example.taskflow.domain.team.exception.TeamErrorCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TeamRepository extends JpaRepository<Team, Long> {
     default Team findByIdOrElseThrow(long id) {
-        return findById(id).orElseThrow(() -> new InvalidTaskException(TeamErrorCode.TEAM_NOT_FOUND));
+        return findById(id).orElseThrow(() -> new InvalidTeamException(TeamErrorCode.TEAM_NOT_FOUND));
     }
+
+    Optional<Team> findByName(String name);
 }
