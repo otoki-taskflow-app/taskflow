24bf024e1da525b40df4469afe6f5547488300b9
assokk <mijisuu@naver.com>
2025-09-04 19:36:24 +0900

    refactor: 사용자 조회 기능 전반 리팩토링
    - teamRepository 직접 접근 제거
    - 타 도메인에서 조회 메서드 사용 시 반환 타입을 User 객체로 변경
    - TeamExternalService로 책임 분할
    - enum 대문자 통일

--- src/main/java/com/example/taskflow/domain/user/service/UserExternalService.java
+++ src/main/java/com/example/taskflow/domain/user/service/UserExternalService.java
@@ -4,7 +4,6 @@ package com.example.taskflow.domain.user.service;
 import com.example.taskflow.common.exception.GlobalException;
 import com.example.taskflow.domain.team.repository.TeamRepository;
 import com.example.taskflow.domain.user.Error.UserErrorCode;
-import com.example.taskflow.domain.user.dto.UserResponse;
 import com.example.taskflow.domain.user.entity.User;
 import com.example.taskflow.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -24,28 +23,21 @@ public class UserExternalService {
 
 
     // 1. 단일 유저 조회 (탈퇴한 유저 제외)
-    public UserResponse getFindById(Long id) {
-        User user = userRepository.findByIdAndDeletedAtIsNull(id)
+    public User getFindById(Long id) {
+        return userRepository.findByIdAndDeletedAtIsNull(id)
                 .orElseThrow(() -> new GlobalException(UserErrorCode.USER_NOT_FOUND));
-        return new UserResponse(user);
     }
 
     // 2. 여러 유저 조회 (탈퇴한 유저 제외)
     // 팀 멤버 목록 조회 시 사용
-    public List<UserResponse> getActiveMemberByTeamId(Long teamId) {
-        if (!teamRepository.existsById(teamId)) {
-            throw new GlobalException(UserErrorCode.TEAM_NOT_FOUND);
-        }
-
+    public List<User> getActiveMemberByTeamId(Long teamId) {
         List<User> users = userRepository.findAllByTeamIdAndDeletedAtIsNull(teamId);
 
         if (users.isEmpty()) {
             throw new GlobalException(UserErrorCode.USER_NOT_FOUND);
         }
 
-        return users.stream()
-                .map(UserResponse::new)
-                .toList();
+        return users;
     }
 
 }
