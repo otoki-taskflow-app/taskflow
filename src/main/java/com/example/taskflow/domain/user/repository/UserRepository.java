fcce0f5ba28248bb2b5885e155a2c082ddd80faa
assokk <mijisuu@naver.com>
2025-09-04 13:53:25 +0900

    feat: 유저 조회 API 컨트롤러 구현

--- src/main/java/com/example/taskflow/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/taskflow/domain/user/repository/UserRepository.java
@@ -12,7 +12,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     // 1. 현재 로그인한 사용자 정보 조회 (deletedAt이 null인 유저만)
     Optional<User> findByIdAndDeletedAtIsNull(Long id);
 
-    // 2. 팀에 속하지 않은 사용자 목록 조회
+    // 2. 팀에 추가 가능한 사용자 목록 조회
     @Query("""
         SELECT u
         FROM User u
@@ -23,5 +23,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
               WHERE tm.team.id = :teamId
           )
         """)
-    List<User> findAvailableUserByTeamId(Long teamId);
+    List<User> findAvailableUsersByTeamId(Long teamId);
 }
