552fa918df6afaf3c5b56831908fbc49be0af456
ksg <kasaga234@gmail.com>
2025-09-04 11:59:31 +0900

    feat(comment): 새로운 comment 에러 코드 추가

--- src/main/java/com/example/taskflow/domain/comment/exception/CommentErrorCode.java
+++ src/main/java/com/example/taskflow/domain/comment/exception/CommentErrorCode.java
@@ -10,8 +10,8 @@ import org.springframework.http.HttpStatus;
 public enum CommentErrorCode implements ErrorCode {
     COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "댓글을 찾을 수 없습니다."),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "댓글 작성자를 찾을 수 없습니다."),
-    TASK_NOT_FOUND(HttpStatus.NOT_FOUND, "댓글이 달릴 Task를 찾을 수 없습니다.");
-
+    TASK_NOT_FOUND(HttpStatus.NOT_FOUND, "댓글이 달릴 Task를 찾을 수 없습니다."),
+    COMMENT_TASK_MISMATCH(HttpStatus.NOT_FOUND, "Task가 일치하지 않습니다.");
     private final HttpStatus httpStatus;
     private final String message;
 }
