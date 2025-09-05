1b1750f01c3b394c3c9e382d4618fa6c37c2bf35
assokk <mijisuu@naver.com>
2025-09-05 17:19:18 +0900

    feat: deletedAt 세터 추가로 auth 탈퇴 로직 연동

--- src/main/java/com/example/taskflow/domain/user/entity/User.java
+++ src/main/java/com/example/taskflow/domain/user/entity/User.java
@@ -51,4 +51,8 @@ public class User extends BaseEntity {
         this.name = name;
         this.role = role;
     }
+
+    public void setDeletedAt(LocalDateTime deletedAt) {
+        this.deletedAt = deletedAt;
+    }
 }
