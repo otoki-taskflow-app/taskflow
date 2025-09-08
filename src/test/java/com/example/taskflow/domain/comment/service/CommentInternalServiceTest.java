b7b5b5889b3c8a82728d8a7a1e125c48774cbfc6
ksg <kasaga234@gmail.com>
2025-09-08 10:34:30 +0900

    feat(comment): service 테스트 코드 추가

---
+++ src/test/java/com/example/taskflow/domain/comment/service/CommentInternalServiceTest.java
@@ -0,0 +1,15 @@
+package com.example.taskflow.domain.comment.service;
+
+import com.example.taskflow.domain.comment.repository.CommentRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class CommentServiceTest {
+    @Mock
+    private CommentRepository commentRepository;
+    @InjectMocks
+    private 
+}
