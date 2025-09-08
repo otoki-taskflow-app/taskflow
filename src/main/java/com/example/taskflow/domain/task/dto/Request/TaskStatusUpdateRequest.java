08bedeea664b0f5646ce704549109410607b5a04
donghyeon505 <ehdgus458@gmail.com>
2025-09-08 12:24:16 +0900

    refactor(task): @NotNull Valid 예외 검증 추가

--- src/main/java/com/example/taskflow/domain/task/dto/Request/TaskStatusUpdateRequest.java
+++ src/main/java/com/example/taskflow/domain/task/dto/Request/TaskStatusUpdateRequest.java
@@ -1,6 +1,10 @@
 package com.example.taskflow.domain.task.dto.Request;
 
 import com.example.taskflow.domain.task.enums.Status;
+import jakarta.validation.constraints.NotNull;
 
-public record TaskStatusUpdateRequest (Status status) {
+public record TaskStatusUpdateRequest (
+        @NotNull
+        Status status
+) {
 }
