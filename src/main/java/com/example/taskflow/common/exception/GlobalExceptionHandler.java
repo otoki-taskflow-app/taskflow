25055a8d38c4766f4bb06b6630604f9e7a89a1bc
donghyeon505 <ehdgus458@gmail.com>
2025-09-08 17:12:52 +0900

    feat(common-exception): Valid 예외처리 응답구조 및 공통 에러 응답 추가

--- src/main/java/com/example/taskflow/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/taskflow/common/exception/GlobalExceptionHandler.java
@@ -2,7 +2,9 @@ package com.example.taskflow.common.exception;
 
 import com.example.taskflow.common.response.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,6 +16,27 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ApiResponse<Object>> handleGlobalException(GlobalException ex) {
         log.error("비즈니스 오류 발생 ", ex);
         return handleExceptionInternal(ex.getErrorCode());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<Object>> handleValidationException(MethodArgumentNotValidException ex) {
+        log.error("유효성 검사 실패 ", ex);
+        String errorMessage = ex.getBindingResult()
+                .getAllErrors()
+                .get(0)
+                .getDefaultMessage();
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ApiResponse.error(errorMessage));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<Object>> handleServerException(Exception ex) {
+        log.error("서버 오류 발생", ex);
+        return ResponseEntity
+                .status(CommonErrorCode.INTERNAL_SERVER_ERROR.getHttpStatus())
+                .body(ApiResponse.error(CommonErrorCode.INTERNAL_SERVER_ERROR));
     }
 
     private ResponseEntity<ApiResponse<Object>> handleExceptionInternal(ErrorCode errorCode) {
