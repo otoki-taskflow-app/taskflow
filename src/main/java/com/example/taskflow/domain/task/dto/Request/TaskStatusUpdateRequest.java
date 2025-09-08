d685820d6871a2650014828f557f457216d613bd
donghyeon505 <ehdgus458@gmail.com>
2025-09-08 17:18:22 +0900

    refactor(task): 어노테이션에 메시지 추가 - String 검증 빼고 @NotNull로 수정

--- src/main/java/com/example/taskflow/domain/task/dto/Request/TaskStatusUpdateRequest.java
+++ src/main/java/com/example/taskflow/domain/task/dto/Request/TaskStatusUpdateRequest.java
@@ -4,7 +4,7 @@ import com.example.taskflow.domain.task.enums.Status;
 import jakarta.validation.constraints.NotNull;
 
 public record TaskStatusUpdateRequest (
-        @NotNull
+        @NotNull(message = "유효하지 않은 상태값입니다.")
         Status status
 ) {
 }
