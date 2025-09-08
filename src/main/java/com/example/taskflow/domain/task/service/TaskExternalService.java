45b4eb9d1974ffe0dbd9c6411c8621209c5140bf
donghyeon505 <ehdgus458@gmail.com>
2025-09-08 12:21:39 +0900

    feat(task): Search 에서 사용할 메서드 추가

--- src/main/java/com/example/taskflow/domain/task/service/TaskExternalService.java
+++ src/main/java/com/example/taskflow/domain/task/service/TaskExternalService.java
@@ -1,10 +1,13 @@
 package com.example.taskflow.domain.task.service;
 
+import com.example.taskflow.domain.search.dto.TaskSummary;
 import com.example.taskflow.domain.task.entity.Task;
 import com.example.taskflow.domain.task.repository.TaskRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
@@ -14,5 +17,13 @@ public class TaskExternalService {
 
     public Task getTaskById(Long taskId) {
         return taskRepository.findByIdAndDeletedAtIsNullOrElseThrow(taskId);
+    }
+
+    public List<TaskSummary> searchByKeyword(String keyword) {
+        return taskRepository
+                .findByTitleContainingIgnoreCaseOrDescriptionContainingIgnoreCase(keyword, keyword)
+                .stream()
+                .map(TaskSummary::from)
+                .toList();
     }
 }
