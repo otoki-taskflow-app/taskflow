e834190d06d2326d4498869b787481d9789916c8
dbdnsdb123 <dbdnsdb123@naver.com>
2025-09-08 20:51:28 +0900

    refactor(Team): ref TeamRepository

--- src/main/java/com/example/taskflow/domain/team/repository/TeamRepository.java
+++ src/main/java/com/example/taskflow/domain/team/repository/TeamRepository.java
@@ -16,7 +16,7 @@ public interface TeamRepository extends JpaRepository<Team, Long> {
 
     Optional<Team> findByName(String name);
 
-    @Query("SELECT DISTINCT t FROM Team t JOIN FETCH t.member tm JOIN FETCH tm.user u WHERE t.id = :id") //@EntityGraph 에러때문에 @Query로 수정
+    @Query("SELECT DISTINCT t FROM Team t LEFT JOIN FETCH t.member tm LEFT JOIN FETCH tm.user u WHERE t.id = :id")
     Optional<Team> findByTeamId(Long id);
 
     List<Team> findByNameContainingIgnoreCaseOrDescriptionContainingIgnoreCase(String nameKeyword, String descriptionKeyword);
