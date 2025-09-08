6367604eb92840873bdc03c12e8f941d06669982
ksg <kasaga234@gmail.com>
2025-09-08 13:20:56 +0900

    feat(commit): request @Notblank 어노테이션 추가

--- src/main/java/com/example/taskflow/domain/comment/dto/request/CommentCreateRequest.java
+++ src/main/java/com/example/taskflow/domain/comment/dto/request/CommentCreateRequest.java
@@ -1,3 +1,7 @@
 package com.example.taskflow.domain.comment.dto.request;
 
-public record CommentCreateRequest(String content) {}
+import jakarta.validation.constraints.NotBlank;
+
+public record CommentCreateRequest(
+        @NotBlank
+        String content) {}
